81a51b35f3d357b305fcc46e44ccd17bdcd67795
Niko-PL <146205724+Niko-PL@users.noreply.github.com>
2025-11-08 17:44:57 +0000

    done some base planning

---
+++ Chrome Extensions/MK1/popup.js
@@ -0,0 +1,17 @@
+console.log("STARTED");
+import Web_APP_URL from './SECRETS.js';
+
+
+/*
+document.getElementById("runBtn").addEventListener("click", async () => {
+    const sheetId = document.getElementById("sheetId").value.trim();
+    const sheetName = document.getElementById("sheetName").value.trim();
+    const clearPrevious = document.getElementById("clearPrevious").checked;
+    const statusDiv = document.getElementById("status");
+
+    const webAppURL = Web_APP_URL;
+
+});
+*/
+
+console.log("Web App URL:", Web_APP_URL);
